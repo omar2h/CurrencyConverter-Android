7b0d5ae37022c2360192ae22a49e65d17c6541e0
omarsamak <omarhesham83@gmail.com>
2020-08-07 09:43:02 +0200

    orientation view added & limit number of digits 9 decimal 2

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -2,21 +2,25 @@ package com.example.currencyconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+
 public class MainActivity extends AppCompatActivity {
 
     EditText in1, in2;
     Spinner spin1, spin2;
-    String[] c = {"United States Dollar", "Egyptian Pound"};
-
 
 
     @Override
@@ -26,11 +30,9 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource( this, R.array.currencies, android.R.layout.simple_spinner_item );
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        
+
         in1 = findViewById(R.id.input1);
         in2 = findViewById(R.id.input2);
-
-
 
         in1.addTextChangedListener(watcher1);
         in2.addTextChangedListener(watcher2);
@@ -41,14 +43,19 @@ public class MainActivity extends AppCompatActivity {
         spin2 = findViewById(R.id.spin2);
         spin2.setAdapter(adapter);
 
+
         spin1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                in1.setText("");
-                in2.setText("");
-                spin2.setSelection((spin1.getSelectedItemPosition()+1)%2);
-                in2.setFocusableInTouchMode(true);
-                in2.requestFocus();
+
+                if(spin1.getSelectedItemPosition()==spin2.getSelectedItemPosition()) {
+                    spin2.setSelection((spin1.getSelectedItemPosition() + 1) % 2);
+                    in1.setText("");
+                    in2.setText("");
+                    in1.requestFocus();
+                }
+                in1.setSelection(in1.getText().length());
+
             }
 
             @Override
@@ -59,12 +66,13 @@ public class MainActivity extends AppCompatActivity {
         spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                in1.setText("");
-                in2.setText("");
-                spin1.setSelection((spin2.getSelectedItemPosition()+1)%2);
-                in1.setFocusableInTouchMode(true);
-                in1.requestFocus();
-
+                if(spin1.getSelectedItemPosition()==spin2.getSelectedItemPosition()) {
+                    spin1.setSelection((spin2.getSelectedItemPosition() + 1) % 2);
+                    in1.setText("");
+                    in2.setText("");
+                    in2.requestFocus();
+                }
+                in2.setSelection(in2.getText().length());
 
             }
 
@@ -73,37 +81,39 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-    }
 
+    }
 
     TextWatcher watcher1 = new TextWatcher() {
 
-        double eg, us;
-
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
         }
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
+            double n;
             String text = spin2.getSelectedItem().toString();
+            in2.removeTextChangedListener(watcher2);
             if(text.equals("Egyptian Pound")) {
-                in2.removeTextChangedListener(watcher2);
                 if (!s.toString().equals("")) {
-                    eg = Double.parseDouble(s.toString());
-                    us = eg * 15.90;
-                    in2.setText(String.format("%.2f", us));
+                    n = Double.parseDouble(s.toString());
+                    n *= 15.90;
+                    if(n%1==0)
+                        in2.setText(String.format("%.0f", n));
+                    else
+                        in2.setText(String.format("%.2f", n));
                 } else
                     in2.setText("");
             }
-            else{
-
-                in2.removeTextChangedListener(watcher2);
+            else if (text.equals("United States Dollar")){
                 if (!s.toString().equals("")) {
-                    us = Double.parseDouble(s.toString());
-                    eg = us / 15.90;
-                    in2.setText(String.format("%.2f", eg));
+                    n = Double.parseDouble(s.toString());
+                    n /= 15.90;
+                    if(n%1==0)
+                        in2.setText(String.format("%.0f", n));
+                    else
+                        in2.setText(String.format("%.2f", n));
                 } else
                     in2.setText("");
             }
@@ -112,12 +122,19 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void afterTextChanged(Editable s) {
             in2.addTextChangedListener(watcher2);
+            String str = in1.getText().toString();
+            if (str.isEmpty()) return;
+            String str2 = PerfectDecimal(str, 9, 2);
+
+            if (!str2.equals(str)) {
+                in1.setText(str2);
+                int pos = in1.getText().length();
+                in1.setSelection(pos);
+            }
         }
     };
 
     TextWatcher watcher2 = new TextWatcher() {
-
-        double eg, us;
 
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -125,23 +142,29 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
+            double n;
             String text = spin1.getSelectedItem().toString();
-            if (text == "United States Dollar") {
-                in1.removeTextChangedListener(watcher1);
+            in1.removeTextChangedListener(watcher1);
+            if(text.equals("Egyptian Pound")) {
                 if (!s.toString().equals("")) {
-                    us = Double.parseDouble(s.toString());
-                    eg = us / 15.90;
-                    in1.setText(String.format("%.2f", eg));
+                    n = Double.parseDouble(s.toString());
+                    n *= 15.90;
+                    if(n%1==0)
+                        in1.setText(String.format("%.0f", n));
+                    else
+                        in1.setText(String.format("%.2f", n));
                 } else
                     in1.setText("");
-            } else {
-                in1.removeTextChangedListener(watcher1);
+            }
+            else if (text.equals("United States Dollar")){
                 if (!s.toString().equals("")) {
-                    eg = Double.parseDouble(s.toString());
-                    us = eg * 15.90;
-                    in1.setText(String.format("%.2f", us));
-                }
-                else
+                    n = Double.parseDouble(s.toString());
+                    n /= 15.90;
+                    if(n%1==0)
+                        in1.setText(String.format("%.0f", n));
+                    else
+                        in1.setText(String.format("%.2f", n));
+                } else
                     in1.setText("");
             }
         }
@@ -149,9 +172,43 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void afterTextChanged(Editable s) {
             in1.addTextChangedListener(watcher1);
+            String str = in2.getText().toString();
+            if (str.isEmpty()) return;
+            String str2 = PerfectDecimal(str, 9, 2);
+
+            if (!str2.equals(str)) {
+                in2.setText(str2);
+                int pos = in2.getText().length();
+                in2.setSelection(pos);
+            }
 
         }
+
     };
+
+    public String PerfectDecimal(String str, int MAX_BEFORE_POINT, int MAX_DECIMAL){
+        if(str.charAt(0) == '.') str = "0"+str;
+        int max = str.length();
+
+        String rFinal = "";
+        boolean after = false;
+        int i = 0, up = 0, decimal = 0; char t;
+        while(i < max){
+            t = str.charAt(i);
+            if(t != '.' && after == false){
+                up++;
+                if(up > MAX_BEFORE_POINT) return rFinal;
+            }else if(t == '.'){
+                after = true;
+            }else{
+                decimal++;
+                if(decimal > MAX_DECIMAL)
+                    return rFinal;
+            }
+            rFinal = rFinal + t;
+            i++;
+        }return rFinal;
+    }
 
 
 }
